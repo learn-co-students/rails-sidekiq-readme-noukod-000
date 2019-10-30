ea189a2cece4358a5134cbb701e8d77d5783a49c
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-10-30 17:37:39 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  
   resources :customers, only: [:index]
   post 'customers/upload', to: 'customers#upload'
 end
